8c9735c59340ca725636ce1d58a8f1fc712ecff2
Jesitha-Searce <jesitha.d@searce.com>
2024-05-28 12:03:54 +0530

    Update pubsubtoFire.java

--- streaming/src/main/java/com/firestore/pubsubtoFire.java
+++ streaming/src/main/java/com/firestore/pubsubtoFire.java
@@ -2,7 +2,6 @@ package com.firestore;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
-import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
@@ -17,35 +16,46 @@ import com.google.gson.JsonSyntaxException;
 
 public class pubsubtoFire {
     public static void main(String[] args) {
-        PipelineOptions options = PipelineOptionsFactory.fromArgs(args).create();
+        Utils options = PipelineOptionsFactory.fromArgs(args).as(Utils.class);
         Pipeline pipeline = Pipeline.create(options);
-        String projectId = "gcp-firestore-423907";
-        String subName = "doc_stream.streaming.streamfrombatch-sub";
-        PCollection<String> messages = pipeline.apply("ReadFromPubSub",
-                PubsubIO.readStrings().fromSubscription("projects/" + projectId + "/subscriptions/" + subName));
+        
+        String projectId=options.getProjectId().get();
+        String databaseName=options.getDatabaseId().get();
+        String collectionName=options.getCollectionName().get();
+        String subscriptionName=options.getSubscriptionName().get();
 
-       messages.apply("ProcessAndSendToFirestore", ParDo.of(new ProcessAndSendToFirestoreFn()));
+        PCollection<String> messages = pipeline.apply("ReadFromPubSub",
+                PubsubIO.readStrings().fromSubscription("projects/" + projectId + "/subscriptions/" + subscriptionName));
+
+        messages.apply("ProcessAndSendToFirestore", ParDo.of(new ProcessAndSendToFirestoreFn(projectId,databaseName,collectionName)));
         pipeline.run();
     }
     static class ProcessAndSendToFirestoreFn extends DoFn<String, Void> {
+        private final String projectId,databaseName,collectionName;
+
+        ProcessAndSendToFirestoreFn(String projectId,String databaseName,String collectionName) {
+            this.projectId=projectId;
+            this.databaseName=databaseName;
+            this.collectionName=collectionName;
+        }
         @ProcessElement
         public void processElement(ProcessContext c) throws Exception {
             FirestoreOptions firestoreOptions = FirestoreOptions.getDefaultInstance().toBuilder()
-                    .setProjectId("gcp-firestore-423907")
-                    .setDatabaseId("firestore-poc")
+                    .setProjectId(projectId)
+                    .setDatabaseId(databaseName)
                     .build();
             Firestore firestore = firestoreOptions.getService();
          String message = c.element();  
-        CollectionReference collection = firestore.collection("stream_check");
+        CollectionReference collection = firestore.collection(collectionName);
         String[] jsonFormat=message.split(",");
         String jsonText=String.join(",",jsonFormat);
         Gson gson=new Gson();
         try{
             try{
                 TypeToken<Map<String, Object>> typeToken = new TypeToken<Map<String, Object>>() {};
-                Map<String, Object> document1 = gson.fromJson(message, typeToken.getType());
-                if (document1.containsKey("after")) {
-                    String afterString = document1.get("after").toString();
+                Map<String, Object> document = gson.fromJson(message, typeToken.getType());
+                if (document.containsKey("after")) {
+                    String afterString = document.get("after").toString();
                     Map<String, Object> afterMap = gson.fromJson(afterString, typeToken.getType());
                     collection.add(afterMap);
                 } else {
